554391ab1971c741b7dd1c81a8ecdd1693e7de28
YonVon <y.lautard@gmail.com>
2016-03-31 20:35:11 +0200

    Recherche des hauts faits dans les catégories
    Ajout d'un loader

--- js/Achievement.js
+++ js/Achievement.js
@@ -1,9 +1,6 @@
 var app = angular.module('achievement',[]);
 
-app.controller('achievementController',function($scope){
-	var data = {};
-	$.getJSON('https://eu.api.battle.net/wow/data/character/achievements?locale=fr_FR&apikey=xyzcvd8sfjx5sketq27e8ttsvdbnttgm', function(dataJSON) {
-			data = dataJSON;
+app.controller('achievementController',function($scope){	var data = {};	$.getJSON('https://eu.api.battle.net/wow/data/character/achievements?locale=fr_FR&apikey=xyzcvd8sfjx5sketq27e8ttsvdbnttgm', function(dataJSON) {			data = dataJSON;			$scope.hiddenLoader = true;
 	    });
 	$scope.seekAchievement = function(){		$scope.resultsAchievements = [];		_.each(data.achievements, function(listeAchievements) {			_.each(listeAchievements.achievements, function(results) {	    		if(!_.isUndefined(data) && !_.isUndefined(results) && !_.isUndefined(results.title)) {	    			if (results.title.indexOf($scope.someVal) >= 0 || results.description.indexOf($scope.someVal) >= 0) {	    			    $scope.resultsAchievements.push(results);	    			}	    		}	  	});			_.each(listeAchievements.categories, function(results) {					_.each(results.achievements, function(results) {						if(!_.isUndefined(data) && !_.isUndefined(results) && !_.isUndefined(results.title)) {	    				if (results.title.indexOf($scope.someVal) >= 0 || results.description.indexOf($scope.someVal) >= 0) {	    			    	$scope.resultsAchievements.push(results);	    				}	    			}	  			});	  	});		});	}
 });
