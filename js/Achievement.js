d438f75d6bfd5b3cfd4ea04136470f7fa9ffe971
YonVon <y.lautard@gmail.com>
2016-03-31 18:55:00 +0200

    Revue seekachievement

--- js/Achievement.js
+++ js/Achievement.js
@@ -1,23 +1,18 @@
-//seekAchievement.js
-
-var app = angular.module('achievement',[]);
-
-app.controller('achievementController',function($scope){
-	var data = {};
-	$.getJSON('https://eu.api.battle.net/wow/data/character/achievements?locale=fr_FR&apikey=xyzcvd8sfjx5sketq27e8ttsvdbnttgm', function(dataJSON) {     
-			data = dataJSON;
-	    });
-	$scope.seekAchievement = function(){
-
-    		/*console.log(data); */
-    		// $scope.resultsAchievement = _.where(data,{title:$scope.someVal});
-    		var results = _.where(data.achievements[0].achievements,{title: $scope.someVal});
-    		if(!_.isUndefined(data) && !_.isUndefined(results) && !_.isUndefined(results[0]) ) {
-    			$scope.resultsAchievement = results.description;
-
-    		}
-
-		
-	}
+var app = angular.module('achievement',[]);
+
+app.controller('achievementController',function($scope){
+	var data = {};
+	$.getJSON('https://eu.api.battle.net/wow/data/character/achievements?locale=fr_FR&apikey=xyzcvd8sfjx5sketq27e8ttsvdbnttgm', function(dataJSON) {     
+			data = dataJSON;
+	    });
+	$scope.seekAchievement = function(){
+        $scope.resultsAchievement = "";
+		_.each(data.achievements[0].achievements, function(results) {
+    		if(!_.isUndefined(data) && !_.isUndefined(results) && !_.isUndefined(results.title)) {
+    			if (results.title.indexOf($scope.someVal) >= 0 || results.description.indexOf($scope.someVal) >= 0) {
+    			     $scope.resultsAchievement = results.title + " - " + results.description;
+    			}
+    		}
+    	});
+	}
 });
-
